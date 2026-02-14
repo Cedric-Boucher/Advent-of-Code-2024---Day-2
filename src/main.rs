b9fc74512f3842be7cda29748bd771889b854acf
Cedric Boucher <cedricjboucher@proton.me>
2026-02-14 21:33:34 +0000

    solve part 2 of the problem

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{error::Error};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Report {
     levels: Vec<u32>,
 }
@@ -22,6 +22,33 @@ impl Report {
         });
 
         (increasing || decreasing) && adjacent_diff
+    }
+
+    fn without_level(&self, index: usize) -> Self {
+        if index < self.levels.len() {
+            let mut report_copy = self.clone();
+            report_copy.levels.remove(index);
+            report_copy
+        }
+        else {
+            self.clone().into()
+        }
+    }
+
+    fn is_safe_with_problem_dampener(&self) -> bool {
+        // A report is considered safe if both of the following are true:
+        // - the levels are either all increasing or all decreasing.
+        // - any two adjacent levels differ by at least one and at most three.
+        // If removing a single level from an unsafe report would make it safe,
+        // the report counts as safe with the problem dampener.
+        if self.is_safe() {
+            true
+        } else {
+            // Iterate through each level and check if removing it would make the report safe.
+            self.levels.iter().enumerate().any(|(i, _)| {
+                self.clone().without_level(i).is_safe()
+            })
+        }
     }
 }
 
@@ -52,7 +79,15 @@ fn main() {
 
     let data: Vec<Report> = import_data(&file_path).unwrap();
 
-    let safe_count: usize = data.into_iter().filter(|report| report.is_safe()).count();
+    // Part 1: Count number of safe reports.
+
+    let safe_count: usize = (&data).into_iter().filter(|report| report.is_safe()).count();
 
     println!("Number of safe reports: {}", safe_count);
+
+    // Part 2: Count number of reports that are safe with the problem dampener.
+
+    let safe_with_problem_dampener_count: usize = (&data).into_iter().filter(|report| report.is_safe_with_problem_dampener()).count();
+
+    println!("Number of reports that are safe with the problem dampener: {}", safe_with_problem_dampener_count);
 }
